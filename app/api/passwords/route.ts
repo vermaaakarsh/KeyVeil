9af02fca842b99401301f9876efd8f622e5c13e0
Aakarsh Verma <developer.aakarshverma@gmail.com>
2025-03-28 09:56:25 +0530

    Added name and category filters to fetch passwords route to refine fetching

--- app/api/passwords/route.ts
+++ app/api/passwords/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import connectDb from "@/lib/mongoose";
 import { withAuth } from "@/lib/with-auth";
 import Password from "@/models/password";
+import { CATEGORY_ENUM } from "@/lib/enums";
 
 const handler = async (request: NextRequest) => {
   try {
@@ -13,20 +14,39 @@ const handler = async (request: NextRequest) => {
     const currentPage = <number>(
       (request.nextUrl.searchParams.get("currentPage") ?? 0)
     );
+    const name = request.nextUrl.searchParams.get("name");
+    const category = request.nextUrl.searchParams.get("category");
     const sortBy = request.nextUrl.searchParams.get("sortBy");
     const sortOrder = request.nextUrl.searchParams.get("sortBy");
 
-    const passwords = await Password.find({
+    const filterObject: {
+      userId: string;
+      isActive: boolean;
+      isDeleted: boolean;
+      category?: CATEGORY_ENUM;
+      name?: object;
+    } = {
+      userId: request.userId,
       isActive: true,
       isDeleted: false,
-    })
+    };
+
+    if (name || name !== "") {
+      filterObject.name = { $regex: name, $options: "i" };
+    }
+    if (category && category !== "null") {
+      filterObject.category = category as unknown as CATEGORY_ENUM;
+    }
+
+    const passwords = await Password.find(filterObject)
       .limit(maxRecordsPerPage)
       .skip(maxRecordsPerPage * (currentPage - 1))
       .sort([[sortBy ?? "name", sortOrder === "desc" ? "desc" : "asc"]])
       .exec();
     const totalPages = Math.ceil(
-      (await Password.countDocuments()) / maxRecordsPerPage
+      (await Password.countDocuments(filterObject)) / maxRecordsPerPage
     );
+
     return NextResponse.json({
       status: "success",
       message: "Passwords fetch successfully!",
