15d9d49c3bbfd671eac5b63be43cc1ab651c8800
Aakarsh Verma <developer.aakarshverma@gmail.com>
2025-03-28 09:58:52 +0530

    Specified the client components by explicitly adding use client

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
 import { FormControl, FormItem, FormLabel, FormMessage } from "./ui/form";
--- components/InfoComponent.tsx
+++ components/InfoComponent.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Info } from "lucide-react";
 
 import {
